d2cf4c8840b7d29cc317a98c37a43f7cfa8e1207
Felix D <flx.descoteaux@gmail.com>
2014-11-22 19:57:36 -0500

    Initial Commit

---
+++ Meetings/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Meetings
+//
+//  Created by FELIX DESCOTEAUX on 2014-11-22.
+//  Copyright (c) 2014 FELIX DESCOTEAUX. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
